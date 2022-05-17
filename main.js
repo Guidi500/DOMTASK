b7e79d92616ca280eb5dd0e486c1657093ce58da
Daniel Morbey <56259453+Guidi500@users.noreply.github.com>
2022-05-17 19:50:49 +0100

    done

--- main.js
+++ main.js
@@ -5,6 +5,17 @@ function roundUp(x) {
 
 function CalculateMagic(a, b) {
     // add your code here
+    let number = (a + 2*b + 90) / 10;
+    if (number % 5 === 0){
+        number -= 8;
+        
+    }
+    else
+    {
+        number += 9;
+        
+    }
+    return roundUp((number ** 2 )/ 3); 
     
     
     // remember to call roundUp() with your result before returning
